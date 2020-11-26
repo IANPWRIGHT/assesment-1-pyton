07ee0f4a97ac128f0a62daa53aecb1dd95751e1f
Ian Wright <ianwright1892@gmail.com>
2020-11-26 15:35:13 +0000

    pricing and order requirements

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+print("Armchair Cost £350 each   Minimum = 1   Maximum = 5")
+print("")
+print("Sofa Cost £500 each   Minimum = 0   Maximum = 3")
+print("")
+print("delivery cost £50")
+print("")
+
+
 arm = int(input("Enter number of Armchair(s) you would like to order :"))
 while arm <1:
   print ("a minimum order of 1 armchar is required")
@@ -6,11 +14,11 @@ while arm >5:
   print ("a maximum of 5 armchaires orderdat a time")
   arm = int(input("Enter number of Armchair(s) you would like to order :"))
 
+
 sof = int(input("Enter mumber of sofa(s) you would like to order:"))
 while sof >3:
   print ("a maximum of 3 sofas orderd at a time")
   arm = int(input("Enter number of sofa(s) you would like to order :"))
-
 
 
 print("costs")
